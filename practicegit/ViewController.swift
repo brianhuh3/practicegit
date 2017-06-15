d69e6d449f11f4117b570d06984159dd5d6c02b9
brianhuh3 <bhuh@brians-macbook-pro.local>
2017-06-14 17:12:10 -0700

    Initial Commit

---
+++ practicegit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practicegit
+//
+//  Created by Brian Huh on 6/14/17.
+//  Copyright © 2017 Brian Huh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
